025560c9cd16390045f780c60e62f7b45661c0fc
Alex Wishart <alexandrawishart@hotmail.com>
2020-12-04 15:12:08 +0000

    Switch to test data

--- src/components/TabView.spec.tsx
+++ src/components/TabView.spec.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import TabView from './TabView';
 import { act } from 'react-dom/test-utils';
-import { tomatoTabs } from '../content/tabInfo';
+import { TabInfo } from '../content/tabInfo';
 
 describe('The TabView component', () => {
   it('should render correctly (with the first tab active)', () => {
-    expect(render(<TabView content={tomatoTabs} />)).toMatchSnapshot();
+    expect(render(<TabView content={testTabs} />)).toMatchSnapshot();
   });
 
   it('should render correctly when a different tab is clicked', () => {
-    const { getAllByRole } = render(<TabView content={tomatoTabs} />);
+    const { getAllByRole } = render(<TabView content={testTabs} />);
 
     const firstTab = getAllByRole('tab')[0];
     const secondTab = getAllByRole('tab')[1];
@@ -23,3 +23,9 @@ describe('The TabView component', () => {
     expect(secondTab).toHaveStyleRule('background', 'none');
   });
 });
+
+const testTabs: TabInfo[] = [
+  { title: '1', content: 'content 1' },
+  { title: '2', content: 'content 2' },
+  { title: '3', content: 'content 3' },
+];
